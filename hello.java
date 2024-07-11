52b7631a83756738cae0f7667c3543de7f59f3de
Yusu <pwc1317@gmail.com>
2024-07-11 11:24:18 +0800

    second commit

--- hello.java
+++ hello.java
@@ -1,5 +1,8 @@
 public class Hello{
   public static void main(String[] args){
      System.out.println("Hello, world!");
+     for(int i = 0; i < 10; i++){
+         System.out.println(i);
+     }
   }
 }
